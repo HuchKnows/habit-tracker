860fe9749bd0795862d8dd51f0ee319b257bdc19
Jason <47354545+HuchKnows@users.noreply.github.com>
2021-06-06 19:38:41 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -12,52 +12,50 @@ from datetime import datetime
 USERNAME = ""
 TOKEN = ""
 GRAPH_ID = "graph1"
-
-today = datetime.now()
-
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 
-user_params = {
-    "token": TOKEN,
-    "username": USERNAME,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes",
-}
-
-"""CREATE A PIXELA USER"""
-# response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
-# print(response.text)
-
-graph_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
-
-graph_config = {
-    "id": GRAPH_ID,
-    "name": "Cycling Graph",
-    "unit": "Km",
-    "type": "float",
-    "color": "ajisai",
-}
-
+today = datetime.now()
 headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-"""CREATE A NEW GRAPH FOR THE NEWLY CREATED USER"""
-# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-# print(response.text)
 
-pixel_creation_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
+def create_user():
+    """create new Pixela user"""
+    print("Create User Selected...")
+    user_params = {
+        "token": input("Token: "),
+        "username": input("Username: "),
+        "agreeTermsOfService": input("Agree to terms of service?(Yes or No) "),  # yes
+        "notMinor": input("Do you agree that you're not a minor?(Yes or No) "),  # yes
+    }
+    response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+    return response.text
 
-pixel_data = {
-    "date": "20210605",
-    "quantity": "9.74",
-}
 
-"""CREATE A PIXEL"""
-# response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
-# print(response.text)
+def create_graph():
+    """create new Pixela graph"""
+    graph_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+    graph_config = {
+        "id": GRAPH_ID,
+        "name": "Cycling Graph",
+        "unit": "Km",
+        "type": "float",
+        "color": "ajisai",
+    }
+    response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+    return response.text
 
-"""EDIT A PIXEL"""
+
+def create_pixel():
+    """create a new pixel"""
+    pixel_creation_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
+    pixel_data = {
+        "date": "20210605",
+        "quantity": "9.74",
+    }
+    response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+    return response.text
 
 
 def edit_pixel():
@@ -70,8 +68,6 @@ def edit_pixel():
     return response.text
 
 
-# print(edit_pixel())
-
 def delete_pixel():
     """deletes a pixel based on date supplied by user input"""
     date = input("Enter the date that you would like to delete(yyyyMMdd): ")
@@ -80,4 +76,29 @@ def delete_pixel():
     return response.text
 
 
-# print(delete_pixel())
+def menu():
+    """main menu"""
+    sel = input("Jason's Pixel console. Please make a selection\n"
+                "\t1. Create a user.\n"
+                "\t2. Create a graph.\n"
+                "\t3. Create a pixel.\n"
+                "\t4. Update a pixel.\n"
+                "\t5. Delete a pixel.\n"
+                "\t6. Quit\n")
+    sel = int(sel)
+    if sel == 1:
+        print(f"Sel: {sel}")
+        return create_user()
+    elif sel == 2:
+        return f"Sel: {sel}", create_graph()
+    elif sel == 3:
+        return f"Sel: {sel}", create_pixel()
+    elif sel == 4:
+        return f"Sel: {sel}", edit_pixel()
+    elif sel == 5:
+        return f"Sel: {sel}", delete_pixel()
+    elif sel == 6:
+        return f"Sel: {sel}", quit()
+
+
+menu()
